b74183a7e4c393926af6583d52755b96810a6f33
lukagraje <138776692+lukagraje@users.noreply.github.com>
2024-06-10 18:24:52 +0200

    pattern

--- src/components/Contacts/ContactForm/ContactForm.jsx
+++ src/components/Contacts/ContactForm/ContactForm.jsx
@@ -34,6 +34,7 @@ export default class ContactForm extends Component {
           id={nameId}
           type="text"
           name="name"
+          pattern="^[a-zA-Z]+(([' \-][a-zA-Z])?[a-zA-Z]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={this.state.name}
@@ -44,6 +45,7 @@ export default class ContactForm extends Component {
           id={numId}
           type="tel"
           name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={this.state.number}
